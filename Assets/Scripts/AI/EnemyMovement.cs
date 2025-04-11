62c76fadb3f7336e28bcc36fa0349ba89fc24f8f
TopAcademy2022 <popov_r_top@outlook.com>
2025-04-11 21:28:43 +0300

    Enemy see player.

--- Assets/Scripts/AI/EnemyMovement.cs
+++ Assets/Scripts/AI/EnemyMovement.cs
@@ -10,14 +10,22 @@ public class EnemyMovement : MonoBehaviour
 
 	private float _stopDistance;
 
-	public EnemyMovement()
-	{
-		this._moveSpeed = 7.0f;
-		this._stopDistance = 2.0f;
-	}
+	private float rayLength = 30.0f;
+
+	private Color rayColor = Color.red;
+
+	private bool _isSee = false;
+
+	private float maxTime = 15.0f;
+
+	private float currentTime;
 
 	private void Awake()
 	{
+		this._moveSpeed = 300.0f;
+		this._stopDistance = 1.0f;
+		currentTime = maxTime;
+
 		this._rigidbody = GetComponent<Rigidbody>();
 		this._destinationTransform = GameObject.FindWithTag("Player").transform;
 	}
@@ -36,8 +44,35 @@ public class EnemyMovement : MonoBehaviour
 		}
 	}
 
+	private void DetectSee()
+	{
+		Ray ray = new Ray(transform.position, transform.forward);
+
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit, rayLength))
+		{
+			if (hit.collider.CompareTag("Player"))
+			{
+				_isSee = true;
+			}
+		}
+
+		Debug.DrawRay(ray.origin, ray.direction * rayLength, rayColor);
+	}
+
 	private void FixedUpdate()
 	{
-		this.MoveOnPoint();
+		DetectSee();
+
+		if (currentTime > 0.0f && _isSee)
+		{
+			this.MoveOnPoint();
+			currentTime -= Time.deltaTime;
+		}
+		else
+		{
+			_isSee = false;
+			currentTime = maxTime;
+		}
 	}
 }
